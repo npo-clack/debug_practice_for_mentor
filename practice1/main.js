116636b3d8633320e0081030c20472f374ac880c
nakaryo <hoge@example.com>
2022-09-27 20:30:18 +0900

    Add README

--- practice1/main.js
+++ practice1/main.js
@@ -1,7 +1,5 @@
 const a = ['あ','い','う','え','お'];
 
 for(let i=0; i < a.length; i++){
-    for(let j=0; j< a.length; j++){
-        document.write(a[i]+ a[j] + '\n');
-    }
+    document.write(a[i]+ '\n');
 }
