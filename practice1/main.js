5c2cf8c5a866b6b4a001b27b1451505c59c587bf
nakaryo <hoge@example.com>
2022-09-27 18:59:58 +0900

    Add sample wrong codes

---
+++ practice1/main.js
@@ -0,0 +1,7 @@
+const a = ['あ','い','う','え','お'];
+
+for(let i=0; i < a.length; i++){
+    for(let j=0; j< a.length; j++){
+        document.write(a[i]+ a[j] + '\n');
+    }
+}
---
+++ practice2/main.js
@@ -0,0 +1,7 @@
+const a = ['あ','い','う','え','お'];
+
+for(let i=1; i <= a.length; i++){
+    for(let j=1; j<= a.length; j++){
+        document.write(a[i]+ a[j] + '\n');
+    }
+}
